373e0823401a14b58bfafcb31b3cb66c3b36bc7d
Matias <bxkick123@gmail.com>
2022-12-18 03:12:22 -0300

    sexo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://api.spacexdata.com/v4/rockets')
+      .subscribe((data: any) => {
+        this.data = data;
+      });
+  }
 }
